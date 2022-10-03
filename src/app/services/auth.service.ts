1b480f4e93d9cc4a090205c844ca7236377a5be0
RahulSG39 <rahulgirish39@gmail.com>
2022-10-03 17:43:57 +0530

    Strong user data and refactoring to service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import IUser from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+
+  public async createUser(userData: IUser) {
+    const userCred = await this.auth.createUserWithEmailAndPassword(
+      userData.email,
+      userData.password
+    );
+
+    await this.db.collection('users').add({
+      name: userData.name,
+      email: userData.email,
+      age: userData.age,
+      phoneNumber: userData.phoneNumber,
+    });
+  }
+}
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from 'src/app/services/auth.service';
+import IUser from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -8,13 +9,12 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  constructor(private auth: AngularFireAuth) {}
-
+  constructor(private auth: AuthService) {}
   inSubmission = false;
 
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl('', [
+  age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
     Validators.max(120),
@@ -48,14 +48,8 @@ export class RegisterComponent {
     this.alertColor = 'blue';
     this.inSubmission = true;
 
-    const { email, password } = this.registerForm.value;
-
     try {
-      const userCred = await this.auth.createUserWithEmailAndPassword(
-        email as string,
-        password as string
-      );
-      console.log(userCred);
+      await this.auth.createUser(this.registerForm.value as IUser);
     } catch (e) {
       console.error(e);
       this.alertMsg = 'An unexpected error occurred. Please try again later';
