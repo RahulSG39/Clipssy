3d138e719811c66aee747faaf1f3062a786981ef
RahulSG39 <rahulgirish39@gmail.com>
2022-10-04 20:45:39 +0530

    Connected user with their data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {}
+  private usersCollection: AngularFirestoreCollection<IUser>;
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.usersCollection = db.collection('users');
+  }
 
   public async createUser(userData: IUser) {
+    if (!userData.password) {
+      throw new Error('Password not provided!');
+    }
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email,
       userData.password
     );
 
-    await this.db.collection('users').add({
+    if (!userCred.user) {
+      throw new Error('User cannot be found!');
+    }
+
+    await this.usersCollection.doc(userCred.user.uid).set({
       name: userData.name,
       email: userData.email,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
+    });
+
+    await userCred.user.updateProfile({
+      displayName: userData.name,
     });
   }
 }
